edf85bc47d73297e529a82ced923a26595a8c5d3
Marharyta Slobodziana <margo.slobodzian@xmlshop.com>
2023-12-27 13:55:50 +0100

    Adding transactions

--- app/Services/Link/LinkDeleteService.php
+++ app/Services/Link/LinkDeleteService.php
@@ -5,14 +5,22 @@ namespace App\Services\Link;
 use App\Http\Controllers\PageAController;
 use App\Models\Link;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class LinkDeleteService
 {
     public static function delete(Link $link)
     {
-        User::where('link_id', $link->id)
-            ->delete();
-        $link->delete();
+        try {
+            DB::beginTransaction();
+            User::where('link_id', $link->id)
+                ->delete();
+            $link->delete();
+            DB::commit();
+        }  catch (\Exception $exception) {
+            DB::rollBack();
+            abort(500);
+        }
         Link::where('updated_at', '<', now()->subWeek())
             ->delete();
     }
--- app/Services/User/UserCreateService.php
+++ app/Services/User/UserCreateService.php
@@ -5,19 +5,26 @@ namespace App\Services\User;
 use App\Models\User;
 use App\Models\Win;
 use App\Services\Link\LinkCreateService;
+use Illuminate\Support\Facades\DB;
 
 class UserCreateService
 {
     public static function create(array $data)
     {
-        $link = LinkCreateService::create();
-        $win = Win::create();
-        $user = User::firstOrCreate(['phone_number' => $data['phone_number']], [
-            'username' => $data['username'],
-            'phone_number' => $data['phone_number'],
-            'link_id' => $link->id,
-            'win_id' => $win->id,
-        ]);
+        try {
+            $link = LinkCreateService::create();
+            $win = Win::create();
+            $user = User::firstOrCreate(['phone_number' => $data['phone_number']], [
+                'username' => $data['username'],
+                'phone_number' => $data['phone_number'],
+                'link_id' => $link->id,
+                'win_id' => $win->id,
+            ]);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            abort(500);
+        }
         return $user;
     }
 }
